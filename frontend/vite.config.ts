f875b3576e39cd2a982e808713ab2e7bda7c3026
Gloriana Peña Ramírez <gloryx@msn.com>
2026-09-22 13:47:46 -0600

    Agrega funcionalidades frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,5 +5,18 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: 8080,
+    allowedHosts: [
+      'agile-renewal-production-2d26.up.railway.app',
+    ],
+  },
 })
